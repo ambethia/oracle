9a3c2cd85ef97c25f21c49ea19317f5af1bef897
Jason L Perry <jasper@ambethia.com>
2015-11-06 22:11:26 -0500

    Hello, React

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var ENVIRONMENT = process.env.NODE_ENV || 'development';
+var ROOT_PATH = path.resolve(__dirname);
+var CLIENT_PATH = path.resolve(ROOT_PATH, 'client');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'public');
+
+var config = {
+  common: {
+    entry: [
+      CLIENT_PATH
+    ],
+    output: {
+      path: BUILD_PATH
+    },
+    plugins: [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(CLIENT_PATH, 'index.tpl.html'),
+        inject: 'body',
+        filename: 'index.html'
+      }),
+      // new webpack.DefinePlugin({
+      //   'process.env.NODE_ENV': ENVIRONMENT
+      // })
+    ],
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        include: [CLIENT_PATH],
+        loaders: ['react-hot', 'babel']
+      }, {
+        test: /\.less$/,
+        loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!less'
+      }, {
+        test: /\.(css)(\?.+)?$/,
+        loader: 'style!css'
+      }, {
+        test: /\.(ttf|eot|svg|otf|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    }
+  },
+
+  development: {
+    devtool: 'eval-source-map',
+    entry: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/dev-server'
+    ],
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ],
+    output: {
+      filename: '[name].js',
+      publicPath: '/'
+    }
+  },
+
+  production: {
+    output: {
+      filename: '[name]-[hash].min.js'
+    },
+    plugins: [
+      new ExtractTextPlugin('[name]-[hash].min.css'),
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false,
+          screw_ie8: true
+        }
+      })
+    ],
+    postcss: [
+      require('autoprefixer')
+    ]
+  }
+}
+
+module.exports = merge(config.common, config[ENVIRONMENT]);
